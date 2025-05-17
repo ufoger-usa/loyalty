8bc80a1d338b7c104f89c8dcb19d44ba844dd59c
ufoger-usa <ufoger@usa.com>
2025-05-17 03:36:17 -0400

    chore: remove docs folder

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Set base path for Netlify/static hosting
+  base: '/loyalty/', // Ensure base path matches GitHub Pages repo name
   build: {
     outDir: 'dist', // Change output directory to 'dist' for GitHub Actions deployment
     rollupOptions: {
